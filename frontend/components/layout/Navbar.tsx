c355741ba2965d827c141f84b0ee5cc88b6cf718
Jannat Jarin <jannatjarin3@gmail.com>
2026-09-08 21:55:30 +0600

    added logout to shared navbar

--- frontend/components/layout/Navbar.tsx
+++ frontend/components/layout/Navbar.tsx
@@ -1,16 +1,40 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
+
+    const router = useRouter();
+
+    const logout = () => {
+
+        localStorage.removeItem(
+            "access_token"
+        );
+
+        localStorage.removeItem(
+            "refresh_token"
+        );
+
+        router.push(
+            "/login"
+        );
+
+    }
+
     return (
         <div className="navbar bg-base-100 shadow-sm px-8">
-            
+
             <div className="flex-1">
+
                 <Link
                     href="/"
                     className="text-xl font-bold"
                 >
                     SQMS
                 </Link>
+
             </div>
 
             <div className="flex gap-2">
@@ -42,6 +66,13 @@ export default function Navbar() {
                 >
                     Register
                 </Link>
+
+                <button
+                    onClick={logout}
+                    className="btn btn-outline"
+                >
+                    Logout
+                </button>
 
             </div>
 
