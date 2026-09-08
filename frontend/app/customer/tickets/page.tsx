ba91358d85c034930c45096911c4680eaf91a74f
anuanika21 <iamanuanika21@gmail.com>
2026-09-09 00:02:13 +0600

    added customer ticket fetching

--- frontend/app/customer/tickets/page.tsx
+++ frontend/app/customer/tickets/page.tsx
@@ -1,18 +1,84 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 interface Ticket {
     id: number,
     ticketNumber: string,
     status: string,
-    priority: string
+    priority: string,
+    issuedAt: string,
+    calledAt: string | null,
+    completedAt: string | null
 }
 
 export default function CustomerTicketsPage() {
 
-    const [tickets] =
-        useState<Ticket[]>([]);
+    const [tickets, setTickets] =
+    useState<Ticket[]>([]);
+
+const [err, setErr] =
+    useState("");
+
+    useEffect(() => {
+
+    const getTickets = async () => {
+
+        const token =
+            localStorage.getItem(
+                "access_token"
+            );
+
+        try {
+
+            const response =
+                await axios.get<Ticket[]>(
+                    "http://localhost:3000/tickets/mytickets",
+                    {
+                        headers: {
+                            Authorization:
+                                `Bearer ${token}`
+                        }
+                    }
+                );
+
+            setTickets(
+                response.data
+            );
+
+            setErr("");
+
+        }
+
+        catch (error) {
+
+            if (
+                axios.isAxiosError(error) &&
+                error.response?.data?.message
+            ) {
+
+                setErr(
+                    error.response.data.message
+                );
+
+            }
+
+            else {
+
+                setErr(
+                    "Could not load tickets"
+                );
+
+            }
+
+        }
+
+    }
+
+    getTickets();
+
+}, []);
 
     return (
         <div className="max-w-6xl mx-auto py-8">
@@ -24,7 +90,16 @@ export default function CustomerTicketsPage() {
             <p className="mb-6">
                 View your queue tickets
             </p>
+{
+    err &&
+    <div className="alert alert-error mb-4">
 
+        <span>
+            {err}
+        </span>
+
+    </div>
+}
             {
                 tickets.length == 0 &&
 
