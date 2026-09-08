30c1361ce7cd6c0b52ef54b450878bf6b7611622
Jannat Jarin <jannatjarin3@gmail.com>
2026-09-08 22:02:12 +0600

    added global loading page

---
+++ frontend/app/loading.tsx
@@ -0,0 +1,12 @@
+import Loading from "@/components/ui/Loading";
+
+export default function AppLoading() {
+
+    return (
+        <div className="min-h-[70vh] flex items-center justify-center">
+
+            <Loading />
+
+        </div>
+    );
+}
