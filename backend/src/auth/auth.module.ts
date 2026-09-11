46592275620853af0900f145efffbaed046d1f67
Naorin-islam <naorinislamrupam6532@gmail.com>
2026-09-11 21:31:25 +0600

    implement AuthModule with JWT authentication and role-based access control guards

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,31 +1,104 @@
-// src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { UsersModule } from 'src/users/users.module';
-import { JwtGuard } from './jwtGuard';
-import { JwtStrategy } from './jwtStrategy';
-import { RolesGuard } from './roles/roles.guard';
-import { MailModule } from 'src/mail/mail.module';
+
+import {
+  JwtModule,
+} from '@nestjs/jwt';
+
+import {
+  ConfigModule,
+  ConfigService,
+} from '@nestjs/config';
+
+import { AuthService } from
+  './auth.service';
+
+import { AuthController } from
+  './auth.controller';
+
+import { UsersModule } from
+  'src/users/users.module';
+
+import { JwtGuard } from
+  './jwtGuard';
+
+import { JwtStrategy } from
+  './jwtStrategy';
+
+import { RolesGuard } from
+  './roles/roles.guard';
+
+import { MailModule } from
+  'src/mail/mail.module';
+
+import { NotificationsModule } from
+  'src/notifications/notifications.module';
 
 @Module({
   imports: [
-    JwtModule.registerAsync({
-      global: true,
-      imports: [ConfigModule],
-      useFactory: (config: ConfigService) => ({
-        secret: config.getOrThrow<string>('JWT_ACCESS_SECRET'),
-        signOptions: { expiresIn: config.getOrThrow<string>('JWT_ACCESS_EXPIRES_IN') as any },
-      }),
-      inject: [ConfigService],
-    }),
+
+    JwtModule.registerAsync(
+      {
+        global:
+          true,
+
+        imports:
+          [
+            ConfigModule,
+          ],
+
+        useFactory:
+          (
+            config:
+              ConfigService,
+          ) => (
+            {
+              secret:
+                config.getOrThrow<string>(
+                  'JWT_ACCESS_SECRET',
+                ),
+
+              signOptions:
+              {
+                expiresIn:
+                  config
+                    .getOrThrow<string>(
+                      'JWT_ACCESS_EXPIRES_IN',
+                    ) as any,
+              },
+            }
+          ),
+
+        inject:
+          [
+            ConfigService,
+          ],
+      },
+    ),
+
     UsersModule,
+
     MailModule,
+
+    NotificationsModule,
   ],
-  controllers: [AuthController],
-  providers: [AuthService, JwtGuard, JwtStrategy, RolesGuard],
-  exports: [JwtGuard, RolesGuard],
+
+  controllers:
+    [
+      AuthController,
+    ],
+
+  providers:
+    [
+      AuthService,
+      JwtGuard,
+      JwtStrategy,
+      RolesGuard,
+    ],
+
+  exports:
+    [
+      JwtGuard,
+      RolesGuard,
+    ],
 })
-export class AuthModule {}
+export class AuthModule { }
