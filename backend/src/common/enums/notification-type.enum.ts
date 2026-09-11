604bb2441265be46bc75b764e01191298cdf4664
Nusrat <nusratsumu2003@gmail.com>
2026-09-11 15:42:34 +0600

    fix notification type

--- backend/src/common/enums/notification-type.enum.ts
+++ backend/src/common/enums/notification-type.enum.ts
@@ -2,4 +2,7 @@ export enum NotificationType {
   REGISTRATION = 'registration',
   PASSWORD_RESET = 'password_reset',
   TICKET_ISSUED = 'ticket_issued',
+  TICKET_CALLED = 'ticket_called',
+  TICKET_COMPLETED = 'ticket_completed',
+  TICKET_CANCELLED = 'ticket_cancelled',
 }
