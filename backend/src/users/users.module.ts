6fc2ad86c043805f368c57f4ec7dae1a1fbc37dc
Naorin-islam <naorinislamrupam6532@gmail.com>
2026-09-11 21:37:52 +0600

    create UsersModule and register Users and Counters entities with TypeORM

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,14 +1,46 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
-import { Users } from './users.entity';
+import {
+  TypeOrmModule,
+} from '@nestjs/typeorm';
+
+import {
+  UsersController,
+} from './users.controller';
+
+import {
+  UsersService,
+} from './users.service';
+
+import {
+  Users,
+} from './users.entity';
+
+import {
+  Counters,
+} from '../counters/counters.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
-  controllers: [UsersController],
-  providers: [UsersService],
-  exports: [UsersService, TypeOrmModule],
+  imports: [
+    TypeOrmModule.forFeature(
+      [
+        Users,
+        Counters,
+      ],
+    ),
+  ],
+
+  controllers: [
+    UsersController,
+  ],
+
+  providers: [
+    UsersService,
+  ],
+
+  exports: [
+    UsersService,
+    TypeOrmModule,
+  ],
 })
-export class UsersModule {}
+export class UsersModule { }
