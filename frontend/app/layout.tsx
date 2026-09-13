4df8b06842aa53645f1745a213058bf4b8c89913
anuanika21 <iamanuanika21@gmail.com>
2026-09-13 21:57:10 +0600

    fixed app layout page

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,49 +1,90 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {
+    Metadata,
+} from "next";
+
+import type {
+    ReactNode,
+} from "react";
+
+import {
+    Geist,
+    Geist_Mono,
+} from "next/font/google";
+
 import "./globals.css";
 
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const geistSans =
+    Geist(
+        {
+            variable:
+                "--font-geist-sans",
 
-export const metadata: Metadata = {
-  title: "SQMS",
-  description: "Smart Queue Management System",
-};
+            subsets: [
+                "latin",
+            ],
+        }
+    );
 
-export default function RootLayout({
-  children,
-}: LayoutProps<"/">) {
 
-  return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+const geistMono =
+    Geist_Mono(
+        {
+            variable:
+                "--font-geist-mono",
 
-      <body className="min-h-full flex flex-col">
+            subsets: [
+                "latin",
+            ],
+        }
+    );
 
-        <Navbar />
 
-        <main className="flex-1 px-5">
+export const metadata:
+    Metadata =
+    {
+        title:
+            "SQMS",
 
-          {children}
+        description:
+            "Smart Queue Management System",
+    };
 
-        </main>
 
-        <Footer />
+export default function RootLayout(
+    {
+        children,
+    }: Readonly<{
+        children: ReactNode;
+    }>
+) {
 
-      </body>
+    return (
+        <html
+            lang="en"
+            className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+        >
 
-    </html>
-  );
+            <body className="min-h-full flex flex-col">
+
+                <Navbar />
+
+
+                <main className="flex-1 px-5">
+
+                    {children}
+
+                </main>
+
+
+                <Footer />
+
+            </body>
+
+        </html>
+    );
+
 }
