2fe5acbc64719bc179389232962bb7a3ab1f6067
Nusrat <nusratsumu2003@gmail.com>
2026-09-13 12:06:56 +0600

    Fix main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,29 +1,97 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './common/http-exception.filter';
+import {
+  ValidationPipe,
+} from '@nestjs/common';
+
+import {
+  NestFactory,
+} from '@nestjs/core';
+
+import {
+  DocumentBuilder,
+  SwaggerModule,
+} from '@nestjs/swagger';
+
+import {
+  AppModule,
+} from './app.module';
+
+import {
+  HttpExceptionFilter,
+} from './common/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-  origin: 'http://localhost:3001',
-});
+  const app =
+    await NestFactory.create(
+      AppModule,
+    );
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      forbidNonWhitelisted: true,
-    }),
+  app.enableCors(
+    {
+      origin:
+        'http://localhost:3001',
+    },
   );
 
-  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe(
+      {
+        whitelist:
+          true,
 
-  await app.listen(process.env.PORT ?? 3000);
+        transform:
+          true,
+
+        forbidNonWhitelisted:
+          true,
+      },
+    ),
+  );
+
+  app.useGlobalFilters(
+    new HttpExceptionFilter(),
+  );
+
+  const swaggerConfig =
+    new DocumentBuilder()
+      .setTitle(
+        'Smart Queue Management System',
+      )
+      .setDescription(
+        'SQMS REST API',
+      )
+      .setVersion(
+        '1.0',
+      )
+      .addBearerAuth()
+      .build();
+
+  const swaggerDocument =
+    SwaggerModule.createDocument(
+      app,
+      swaggerConfig,
+    );
+
+  SwaggerModule.setup(
+    'api',
+    app,
+    swaggerDocument,
+  );
+
+  const port =
+    process.env.PORT ??
+    3000;
+
+  await app.listen(
+    port,
+  );
 
   console.log(
-    `Application running on http://localhost:${process.env.PORT ?? 3000}`,
+    `Application running on http://localhost:${port}`,
+  );
+
+  console.log(
+    `Swagger running on http://localhost:${port}/api`,
   );
 }
 
